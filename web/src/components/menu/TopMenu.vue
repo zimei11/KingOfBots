e7de3f2b19c1133f040af1aa1bca0bc8ad9081a4
zimei li <2928577064@qq.com>
2022-07-13 10:49:33 +0800

    菜单配置完成

--- web/src/components/menu/TopMenu.vue
+++ web/src/components/menu/TopMenu.vue
@@ -7,22 +7,22 @@
     <el-sub-menu index="2" class="hidden-md-and-up">
       <template #title>菜单</template>
       <el-menu-item index="/">首页</el-menu-item>
-      <el-menu-item index="2.2">对战</el-menu-item>
-      <el-menu-item index="2.3">对战列表</el-menu-item>
-      <el-menu-item index="2.4">排行榜</el-menu-item>
+      <el-menu-item index="/pk/" route="/pk/">对战</el-menu-item>
+      <el-menu-item index="/record/" route="/record/">对战列表</el-menu-item>
+      <el-menu-item index="/ranklist/" route="/ranklist/">排行榜</el-menu-item>
     </el-sub-menu>
 
     <!--大尺寸菜单响应式-->
     <el-menu-item index="/" class="hidden-sm-and-down">首页</el-menu-item>
-    <el-menu-item index="4" class="hidden-sm-and-down">对战</el-menu-item>
-    <el-menu-item index="5" class="hidden-sm-and-down">对局列表</el-menu-item>
-    <el-menu-item index="6" class="hidden-sm-and-down">排行榜</el-menu-item>
+    <el-menu-item index="/pk/" class="hidden-sm-and-down" route="/pk/">对战</el-menu-item>
+    <el-menu-item index="/record/" class="hidden-sm-and-down" route="/record/">对局列表</el-menu-item>
+    <el-menu-item index="/ranklist/" class="hidden-sm-and-down" route="/ranklist/">排行榜</el-menu-item>
 
     <div class="flex-grow"></div>
 
     <el-sub-menu index="3" >
       <template #title>ZiMei</template>
-      <el-menu-item index="3.1">我的Bot</el-menu-item>
+      <el-menu-item index="/user/bot/" route="/user/bot/">我的Bot</el-menu-item>
       <el-menu-item index="3.2">退出</el-menu-item>
     </el-sub-menu>
 
