6e1a9ce031942d98d732bf25f56804baf11954d7
zimei li <2928577064@qq.com>
2022-07-14 18:59:50 +0800

    主页轮播图纯色

--- web/src/components/home/HomePicture.vue
+++ web/src/components/home/HomePicture.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-carousel :interval="5000" arrow="always" class="picture">
+  <el-carousel :interval="5000" arrow="always" class="picture" height="100px">
     <el-carousel-item v-for="item in 4" :key="item">
       <h3 text="2xl" justify="center">{{ item }}</h3>
     </el-carousel-item>
@@ -16,20 +16,21 @@ export default {
 .el-carousel__item h3 {
   color: #475669;
   opacity: 0.75;
-  line-height: 300px;
+  line-height: 100px;
   margin: 0;
   text-align: center;
 }
 
 .el-carousel__item:nth-child(2n) {
-  background-color: #99a9bf;
+  background: linear-gradient(90deg, #8EC5FC 0%, #E0C3FC 33%, #e0c3fc 66%, #8ec5fc 100%);
 }
 
 .el-carousel__item:nth-child(2n + 1) {
-  background-color: #d3dce6;
+  background-image: linear-gradient(90deg, #9599E2 0%, #8BC6EC 33%, #8bc6ec 66%, #9599e2 100%);
 }
 
 .picture{
   margin-bottom: 20px;
+
 }
 </style>
