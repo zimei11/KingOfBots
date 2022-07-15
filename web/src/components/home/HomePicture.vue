392cefa9d33c45f502e245aa9e4a664fc4949e59
zimei li <2928577064@qq.com>
2022-07-15 17:37:56 +0800

    轮播文字完成

--- web/src/components/home/HomePicture.vue
+++ web/src/components/home/HomePicture.vue
@@ -1,14 +1,24 @@
 <template>
-  <el-carousel :interval="5000" arrow="always" class="picture" height="100px">
-    <el-carousel-item v-for="item in 4" :key="item">
-      <h3 text="2xl" justify="center">{{ item }}</h3>
+  <el-carousel :interval="5000" arrow="always" class="picture" height="50px" direction="vertical">
+    <el-carousel-item v-for="item in message" :key="item.url">
+      <h3 text="2xl" justify="center"><el-link type="default" class="link" :href="item.url">{{ item.text }}</el-link></h3>
     </el-carousel-item>
   </el-carousel>
 </template>
 
 <script>
+
 export default {
-  name: "HomePicture"
+  name: "HomePicture",
+  data() {
+    return {
+      message: [
+        {text: "今日热门游戏", url: "https://www.baidu.com", id: 1},
+        {text: "这款AI居然战无不胜ˋ( ° ▽、° ) ", url: "https://www.baidu.com", id: 2},
+        {text: "打赏下孩子吧(￣﹃￣)", url: "https://www.baidu.com", id: 3},
+      ]
+    }
+  },
 }
 </script>
 
@@ -16,7 +26,7 @@ export default {
 .el-carousel__item h3 {
   color: #475669;
   opacity: 0.75;
-  line-height: 100px;
+  line-height: 50px;
   margin: 0;
   text-align: center;
 }
@@ -29,8 +39,13 @@ export default {
   background-image: linear-gradient(90deg, #9599E2 0%, #8BC6EC 33%, #8bc6ec 66%, #9599e2 100%);
 }
 
-.picture{
+.picture {
   margin-bottom: 20px;
 
+}
+
+.link{
+  color: #222222;
+  font-size: 18px;
 }
 </style>
