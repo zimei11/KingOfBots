23a7ebb89efa4b3ddb688fd67255c74da89d2b1e
zimei li <2928577064@qq.com>
2022-07-29 17:15:53 +0800

    前端界面登录授权

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -7,47 +7,72 @@ import UserBotIndexView from "@/views/user/bot/UserBotIndexView";
 import NotFound from "@/views/error/NotFound";
 import UserAccountLoginView from "@/views/user/account/UserAccountLoginView";
 import UserAccountRegisterView from "@/views/user/account/UserAccountRegisterView";
+import store from "@/store/index";
 
 const routes = [
     {
         path: '/',
         name: 'home',
-        component: HomeView
+        component: HomeView,
+        meta:{
+            requestAuth : false
+        }
     },
     {
         path: "/pk/",
         name: "pk_index",
         component: PkIndexView,
+        meta:{
+            requestAuth : true
+        }
     },
     {
         path: "/record/",
         name: "record_index",
         component: RecordIndexView,
+        meta:{
+            requestAuth : true
+        }
     },
     {
         path: "/ranklist/",
         name: "ranklist_index",
         component: RanklistIndexView,
+        meta:{
+            requestAuth : true
+        }
     },
     {
         path: "/user/bot/",
         name: "user_bot_index",
         component: UserBotIndexView,
+        meta:{
+            requestAuth : true
+        }
     },
     {
         path:"/user/account/login/",
         name:"user_account_login",
         component: UserAccountLoginView,
+        meta:{
+            requestAuth : false
+        }
     },
     {
         path: "/user/account/register/",
         name: "user_account_register",
         component: UserAccountRegisterView,
+        meta:{
+            requestAuth : false
+        }
     },
     {
         path: "/404/",
         name: "404",
         component: NotFound,
+        meta:{
+            requestAuth : false
+        }
     },
     {
         path: "/:catchAll(.*)",
@@ -58,6 +83,15 @@ const routes = [
 const router = createRouter({
     history: createWebHistory(),
     routes
+})
+
+router.beforeEach((to,from,next)=>{
+//需要授权且用户没有登录
+    if(to.meta.requestAuth&&!store.state.user.is_login){
+        next({name:"user_account_login"});
+    }else{
+        next();
+    }
 })
 
 export default router
