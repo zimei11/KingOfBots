6c175f4fad25404f15891b42d35a907dd316c357
zimei li <2928577064@qq.com>
2022-08-03 13:05:42 +0800

    删除user bot功能完成

--- web/src/components/userbot/BotList.vue
+++ web/src/components/userbot/BotList.vue
@@ -8,18 +8,21 @@
 
       </div>
 
-      <el-table :data="bots"  stripe style="width: 100%">
+      <el-table :data="bots"  stripe style="width: 100%"
+      >
         <el-table-column fixed prop="title" label="Bot名称" width="120"/>
         <el-table-column prop="description" label="简介" width="300"/>
         <el-table-column prop="rating" label="积分" width="120"/>
         <el-table-column prop="createTime" label="创建时间" width="165"/>
         <el-table-column prop="modifyTime" label="修改时间" width="165"/>
         <el-table-column fixed="right" label="操作" width="120">
-          <template #default>
-            <el-button link type="primary" @click="handleClick">
+          <template #default="scope">
+            <el-button link type="primary" @click="updateBot(scope.row)">
               修改
             </el-button>
-            <RemoveBot :bots="bots" @refresh_bots="refresh_bots"></RemoveBot>
+            <el-button link type="danger" @click="removeBot(scope.row)">
+              删除
+            </el-button>
           </template>
         </el-table-column>
       </el-table>
@@ -32,11 +35,11 @@ import {ref} from "vue";
 import {useStore} from "vuex";
 import $ from 'jquery';
 import AddBot from "@/components/userbot/botlistdetail/AddBot";
-import RemoveBot from "@/components/userbot/botlistdetail/RemoveBot";
+
 
 export default {
   name: "BotList",
-  components: {RemoveBot, AddBot},
+  components: { AddBot},
   setup() {
     const store = useStore();
     let bots = ref([]);
@@ -56,14 +59,34 @@ export default {
 
     refresh_bots();
 
-    const handleClick = () => {
+    const updateBot = () => {
       console.log('click')
     }
 
+    const removeBot=(row)=>{
+      //console.log(row);
+      $.ajax({
+        url: ("http://localhost:3000/user/bot/remove/"),
+        type: "post",
+        data: {
+          bot_id: row.id,
+        },
+        headers: {
+          'Authorization': "Bearer " + store.state.user.token,
+        },
+        success(resp) {
+          if (resp.error_message === "success") {
+            refresh_bots();
+          }
+        }
+      })
+    }
+
     return {
-      handleClick,
+      updateBot,
       bots,
       refresh_bots,
+      removeBot
     }
   }
 }
