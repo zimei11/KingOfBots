a77166368f33ad6532c67b470a3d45d038265a06
zimei li <2928577064@qq.com>
2022-08-10 22:50:46 +0800

    匹配系统上完成

--- web/src/views/pk/PkIndexView.vue
+++ web/src/views/pk/PkIndexView.vue
@@ -12,33 +12,37 @@ import TopMenu from "@/components/menu/TopMenu";
 import {onMounted, onUnmounted} from "vue";
 import {useStore} from 'vuex'
 import MatchGround from "@/components/MatchGround";
+import {ElNotification} from 'element-plus'
 
 const store = useStore();
 const socketUrl = `ws://localhost:3000/websocket/${store.state.user.token}/`;
 
 let socket = null;
 onMounted(() => {
-  store.commit("updateOpponent",{
-    username:"我的对手",
-    photo:"https://cdn.acwing.com/media/article/image/2022/08/09/1_1db2488f17-anonymous.png"
+  store.commit("updateOpponent", {
+    username: "我的对手",
+    photo: "https://cdn.acwing.com/media/article/image/2022/08/09/1_1db2488f17-anonymous.png"
   })
   socket = new WebSocket(socketUrl);
   socket.onopen = () => {
     // console.log("connected");
-    store.commit("updateSocket",socket);
+    store.commit("updateSocket", socket);
   }
 
   socket.onmessage = msg => {
     const data = JSON.parse(msg.data);
-    if(data.event==="start-matching"){//匹配成功
-      store.commit("updateOpponent",{
-        username:data.opponent_username,
-        photo:data.opponent_photo,
+    if (data.event === "start-matching") {//匹配成功
+      store.commit("updateOpponent", {
+        username: data.opponent_username,
+        photo: data.opponent_photo,
       });
-      setTimeout(()=>{
-        store.commit("updateStatus","playing");
-      },2000);
-      store.commit("updateGamemap",data.gamemap);
+
+      successMessage();
+
+      setTimeout(() => {
+        store.commit("updateStatus", "playing");
+      }, 2000);
+      store.commit("updateGamemap", data.gamemap);
     }
   }
 
@@ -47,10 +51,18 @@ onMounted(() => {
   }
 });
 
-onUnmounted(()=>{
+onUnmounted(() => {
   socket.close();
-  store.commit("updateStatus","matching");
-})
+  store.commit("updateStatus", "matching");
+});
+
+const successMessage = () => {
+  ElNotification({
+    title: '匹配成功',
+    message: '两秒后游戏开始！╰(￣ω￣ｏ)',
+    type: 'success',
+  })
+};
 </script>
 
 <style scoped>
